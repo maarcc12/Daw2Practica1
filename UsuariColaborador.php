672a472d5109551f5c44efb0bcc2683ec9d52fc5
maarcc12 <150950675+maarcc12@users.noreply.github.com>
2024-10-07 19:34:43 +0200

    a

---
+++ UsuariColaborador.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <strong>
+        <h2>Anna Folqué Clua</h2>
+        <h2>Marc Peña Claret</h2>
+    </strong>
+    <br>
+
+
+    <b>07/10/2024</b>
+    <br>
+   
+    <b>19:14</b>
+    <br>
+</body>
+</html>
---
+++ prova3.php
@@ -0,0 +1,16 @@
+<?php
+         $favcolor="verd";
+         switch ($favcolor) {
+          case "vermell":
+          case "VERMELL":
+              echo "el color es vermell!";
+              break;
+          case "blau":
+              echo "el color es blau!";
+              break;
+          case "verd":
+              echo "el color es verd!";
+              break;
+          default:
+              echo "Un altre color!";
+         }
